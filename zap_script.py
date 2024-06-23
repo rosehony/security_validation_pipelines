eb75051be7c5a5daffb258ea6b4e7a2d8051695f
R M Roshany <99073229+rosehony@users.noreply.github.com>
2024-06-23 11:24:39 +0530

    Update zap_script.py

--- zap_script.py
+++ zap_script.py
@@ -1,10 +1,32 @@
-import subprocess
+import time
+from zapv2 import ZAPv2
 
 def run_zap_scan():
-    # Running the ZAP scan
-    subprocess.run(['zap-cli', '--zap-url', 'http://localhost:8080', 'quick-scan', 'http://example.com'], check=True)
-    # Generating the report
-    subprocess.run(['zap-cli', '--zap-url', 'http://localhost:8080', 'report', '-o', 'zap_report.html', '-f', 'html'], check=True)
+    target = 'http://example.com'
+    zap = ZAPv2()
+
+    # Start a new session
+    print('Accessing target {}'.format(target))
+    zap.urlopen(target)
+    time.sleep(2)  # Give the scanner time to start
+
+    print('Spidering target {}'.format(target))
+    scan_id = zap.spider.scan(target)
+    while int(zap.spider.status(scan_id)) < 100:
+        print('Spider progress %: {}'.format(zap.spider.status(scan_id)))
+        time.sleep(2)
+    print('Spider completed')
+
+    print('Scanning target {}'.format(target))
+    scan_id = zap.ascan.scan(target)
+    while int(zap.ascan.status(scan_id)) < 100:
+        print('Scan progress %: {}'.format(zap.ascan.status(scan_id)))
+        time.sleep(5)
+    print('Scan completed')
+
+    # Generate report
+    with open('zap_report.html', 'w') as f:
+        f.write(zap.core.htmlreport())
 
 if __name__ == "__main__":
     run_zap_scan()
