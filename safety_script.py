86fa59f2428a85be0e3280d5086d90e12531c658
R M Roshany <99073229+rosehony@users.noreply.github.com>
2024-06-23 14:21:00 +0530

    Update safety_script.py

--- safety_script.py
+++ safety_script.py
@@ -1,10 +1,16 @@
 import subprocess
+import os
 
 def run_safety():
-    result = subprocess.run(['safety', 'check', '-r', 'test_web/requirements.txt'], capture_output=True, text=True)
-    print(result.stdout)
-    if result.returncode != 0:
-        raise Exception("Dependency issues found")
+    requirements_file = 'test_web/requirements.txt'
+    if os.path.exists(requirements_file):
+        result = subprocess.run(['safety', 'check', '-r', requirements_file], capture_output=True, text=True)
+        print(result.stdout)
+        if result.returncode != 0:
+            raise Exception("Dependency issues found")
+    else:
+        print(f"{requirements_file} not found, skipping dependency scanning.")
 
 if __name__ == "__main__":
     run_safety()
+
